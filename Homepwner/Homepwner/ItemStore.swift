79c0f690e4ef19d38cdaec51bb4dee8a2a8635eb
artimer <leolixiao1@gmail.com>
2017-12-21 14:43:25 +0800

    Deleting Rows, Moving Rows & Displaying User Alerts

--- Homepwner/Homepwner/ItemStore.swift
+++ Homepwner/Homepwner/ItemStore.swift
@@ -19,6 +19,25 @@ class ItemStore {
         
         return newItem
     }
+    
+    func removeItem(item: Item) {
+        if let index = allItems.index(of: item) {
+            allItems.remove(at: index)
+        }
+    }
+    
+    func moveItemAtIndex(fromIndex: Int, toIndex: Int) {
+        if fromIndex == toIndex {
+            return
+        }
+        
+        let movedItem = allItems[fromIndex]
+        
+        allItems.remove(at: fromIndex)
+        
+        allItems.insert(movedItem, at: toIndex)
+    }
+    
 }
 
 
